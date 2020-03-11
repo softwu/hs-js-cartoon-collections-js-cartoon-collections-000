c4c0c682c9b1f25f86560786c6b8e61b41fbabd0
Sheng Chen <eskeisee@gmail.com>
2020-03-11 04:37:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+  var str = "";
+  for(let i = 0; i < dwarves.length; i++){
+    str += `${i+1}. ${dwarves[i]} `
+  }
+  return str;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arr = []
+  
+  for(let i = 0; i < planeteerCalls.length; i++){
+    arr.push(planeteerCalls[i].toUpperCase()+"!")
+  }
+  return arr;
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+    if(words[i].length > 4) return true; 
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camembert"]
+  
+  for(let i = 0; i < foods.length; i++){
+    for(let j = 0; j < cheese.length; j++){
+      if(foods[i] === cheese[j]) return foods[i];
+    }
+  }
+  return "no cheese!";
 }
